97dd3c8962570782a46833c3e54ab657f4cb3515
v3nCll <craftarrow37@gmail.com>
2024-10-11 18:16:03 +0200

    igen

---
+++ májashusi20241011.py
@@ -0,0 +1,28 @@
+print("Welcome", "To", "Logiscool", sep=", ", end="\n_______________\n")
+print("You are welcomed here.", end="\n")
+
+
+a = 42
+b = "apple"
+c = True
+print(a, b, c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+print(123)
+print(0o123)
+print(0x123)
+print(bin(123))
+print(0b101101)
+
+
+a = 0.0002
+b = 2e-4
+print(a)
+print(b)
+print(0.0000000000000000000000001)
+
+
